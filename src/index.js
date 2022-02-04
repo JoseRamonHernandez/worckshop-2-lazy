e1c092e8807217652c064f3a6cf1c4e50fe38ae3
José Ramón Hernández Muñoz <joseramonhernandezmunoz20@gmail.com>
2022-02-04 14:19:16 -0600

    intersection Observer

--- src/index.js
+++ src/index.js
@@ -1,36 +1,38 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+import {registerImage} from  './lazy' 
 
-console.log('Happy hacking :)')
-const maxium = 122;
-const minium = 1;
-const random = () => Math.floor(Math.random() * (maxium - minium)) + minium;
-// crear imagen
+
+const minimum = 1;
+const maximum = 122;
+const random = () => Math.floor(Math.random() * (maximum - minimum)) + minimum
+
 const createImageNode = () => {
+const container = document.createElement('div')
+container.className = "p-4";
 
-    const container = document.createElement('div');
-    container.className = "p-4";
 
-    const imagen = document.createElement('img');
-    imagen.className = "mx-auto";
-    imagen.width = '320';
-    imagen.src = `https://randomfox.ca/images/${random()}.jpg`;
 
-    container.appendChild(imagen)
 
-    return container;
+const imagen = document.createElement("img")
+imagen.className = "mx-auto";
+imagen.width = '320'
+imagen.dataset.src = `https://randomfox.ca/images/${random()}.jpg`;
+
+container.appendChild(imagen);
+
+
+
+return container;
 };
 
 const nuevaImagen = createImageNode()
-const mountNode = document.getElementById("images");
+const mountNode = document.getElementById("images")
 
-const addButton = document.querySelector('button');
+const addButton = document.querySelector("button");
 const addImage = () => {
     const newImage = createImageNode();
-    mountNode.append(newImage);
+    
+mountNode.append(newImage);
+    registerImage(newImage);
 };
-
 
 addButton.addEventListener("click", addImage);
