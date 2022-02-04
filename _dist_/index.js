ee3ae0125730aca65e7d91052ca565a730fee558
JoseRamonHernandez <JoseRamonHernandez@users.noreply.github.com>
2022-02-04 22:35:16 +0000

    deploy: aa54b33b401cc89f2cbafbfd9a8683090f1558d6

--- _dist_/index.js
+++ _dist_/index.js
@@ -4,3 +4,33 @@
  **/
 
 console.log('Happy hacking :)')
+const maxium = 122;
+const minium = 1;
+const random = () => Math.floor(Math.random() * (maxium - minium)) + minium;
+// crear imagen
+const createImageNode = () => {
+
+    const container = document.createElement('div');
+    container.className = "p-4";
+
+    const imagen = document.createElement('img');
+    imagen.className = "mx-auto";
+    imagen.width = '320';
+    imagen.src = `https://randomfox.ca/images/${random()}.jpg`;
+
+    container.appendChild(imagen)
+
+    return container;
+};
+
+const nuevaImagen = createImageNode()
+const mountNode = document.getElementById("images");
+
+const addButton = document.querySelector('button');
+const addImage = () => {
+    const newImage = createImageNode();
+    mountNode.append(newImage);
+};
+
+
+addButton.addEventListener("click", addImage);
